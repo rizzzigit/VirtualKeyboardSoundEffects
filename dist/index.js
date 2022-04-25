c3d4197e9f2cfbb6ad04cad1694a41791009b400
Rizzzi Git <rizzzigit@gmail.com>
2022-04-25 21:41:46 +0800

    update script

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,9 @@ const fs_1 = tslib_1.__importDefault(require("fs"));
 const path_1 = tslib_1.__importDefault(require("path"));
 const child_process_1 = tslib_1.__importDefault(require("child_process"));
 const inputPath = '/dev/input/by-path';
+function playSound(path) {
+    child_process_1.default.exec(`aplay ${path}`, (error) => error && console.error(error));
+}
 async function listInputs() {
     return (await fs_1.default.promises.readdir(inputPath)).map((inputFile) => path_1.default.join(inputPath, inputFile));
 }
@@ -17,12 +20,6 @@ async function findMousePath() {
 function getMouseSoundFile(action) {
     return path_1.default.join(process.cwd(), `res/mouse/${action}.wav`);
 }
-async function mouseData(data) {
-    const dataHex = data.toString('hex');
-    if (['080000', '090000', '0a0000'].includes(dataHex)) {
-        child_process_1.default.exec(`aplay ${getMouseSoundFile(dataHex === '080000' ? 'up' : 'down')}`);
-    }
-}
 function getKeyboardSoundFile(index, action) {
     return path_1.default.join(process.cwd(), `res/keyboard/variant${[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 203, 200, 208, 205, 11, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 87, 88, 210, 211].includes(index)
         ? 1
@@ -32,23 +29,23 @@ function getKeyboardSoundFile(index, action) {
                 ? 3
                 : 4}/key${action}.wav`);
 }
+async function mouseData(data) {
+    const dataHex = data.toString('hex');
+    if (['080000', '090000', '0a0000'].includes(dataHex)) {
+        playSound(getMouseSoundFile(dataHex === '080000' ? 'up' : 'down'));
+    }
+}
 async function keyboardData(data) {
     const action = data[44];
     const key = data[20];
     if ([1, 0].includes(action)) {
-        child_process_1.default.exec(`aplay ${getKeyboardSoundFile(key, !action ? 'down' : 'up')}`, (error) => error && console.log(error));
+        playSound(getKeyboardSoundFile(key, !action ? 'down' : 'up'));
     }
 }
 async function run() {
     const keyboardPath = await findKeyboardPath();
     const mousePath = await findMousePath();
-    if (mousePath) {
-        const mouseStream = fs_1.default.createReadStream(mousePath);
-        mouseStream.on('data', mouseData);
-    }
-    if (keyboardPath) {
-        const keyboardStream = fs_1.default.createReadStream(keyboardPath);
-        keyboardStream.on('data', keyboardData);
-    }
+    mousePath && fs_1.default.createReadStream(mousePath).on('data', mouseData);
+    keyboardPath && fs_1.default.createReadStream(keyboardPath).on('data', keyboardData);
 }
 run();
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import ChildProcess from 'child_process'
 
 const inputPath = '/dev/input/by-path'
 
+function playSound (path: string) {
+  ChildProcess.exec(`aplay ${path}`, (error) => error && console.error(error))
+}
+
 async function listInputs () {
   return (await FS.promises.readdir(inputPath)).map((inputFile) => Path.join(inputPath, inputFile))
 }
@@ -20,14 +24,6 @@ function getMouseSoundFile (action: 'down' | 'up') {
   return Path.join(process.cwd(), `res/mouse/${action}.wav`)
 }
 
-async function mouseData (data: Buffer) {
-  const dataHex = data.toString('hex')
-
-  if (['080000', '090000', '0a0000'].includes(dataHex)) {
-    ChildProcess.exec(`aplay ${getMouseSoundFile(dataHex === '080000' ? 'up' : 'down')}`)
-  }
-}
-
 function getKeyboardSoundFile (index: number, action: 'down' | 'up') {
   return Path.join(process.cwd(), `res/keyboard/variant${
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 203, 200, 208, 205, 11, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 87, 88, 210, 211].includes(index)
@@ -40,12 +36,20 @@ function getKeyboardSoundFile (index: number, action: 'down' | 'up') {
   }/key${action}.wav`)
 }
 
+async function mouseData (data: Buffer) {
+  const dataHex = data.toString('hex')
+
+  if (['080000', '090000', '0a0000'].includes(dataHex)) {
+    playSound(getMouseSoundFile(dataHex === '080000' ? 'up' : 'down'))
+  }
+}
+
 async function keyboardData (data: Buffer) {
   const action = data[44]
   const key = data[20]
 
   if ([1, 0].includes(action)) {
-    ChildProcess.exec(`aplay ${getKeyboardSoundFile(key, !action ? 'down' : 'up')}`, (error) => error && console.log(error))
+    playSound(getKeyboardSoundFile(key, !action ? 'down' : 'up'))
   }
 }
 
@@ -53,17 +57,8 @@ async function run () {
   const keyboardPath = await findKeyboardPath()
   const mousePath = await findMousePath()
 
-  if (mousePath) {
-    const mouseStream = FS.createReadStream(mousePath)
-
-    mouseStream.on('data', mouseData)
-  }
-
-  if (keyboardPath) {
-    const keyboardStream = FS.createReadStream(keyboardPath)
-
-    keyboardStream.on('data', keyboardData)
-  }
+  mousePath && FS.createReadStream(mousePath).on('data', mouseData)
+  keyboardPath && FS.createReadStream(keyboardPath).on('data', keyboardData)
 }
 
 run()
